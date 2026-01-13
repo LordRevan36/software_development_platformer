c6eec61a9c6b7f516753f4a6ce9ab162e26679f6
WorseThanBetter <dereudav000@hsestudents.org>
2026-01-13 14:35:01 -0500

    Parallax Testing, File org.
    haha

---
+++ software-development-platformer/Scenes/camera.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+var lerp_speed = 0.95
+
+@onready var player: CharacterBody2D = $".."
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	offset = offset.lerp(player.velocity/2+Vector2(0,-50), delta * lerp_speed)
