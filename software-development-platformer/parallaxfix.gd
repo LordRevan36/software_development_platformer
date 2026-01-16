eca8eadab9363192b89d847e4514bdf2c241fd0c
WorseThanBetter <dereudav000@hsestudents.org>
2026-01-15 22:28:01 -0500

    PLUGIN THAT FIXES MY PROBLEM??
    -Click on any node2d
    -In the top of the editor, you should see a thingy that says parallax2d preview
    -Do the accurate preview
    omg hopefully this works

--- software-development-platformer/parallaxfix.gd
+++ software-development-platformer/parallaxfix.gd
@@ -2,8 +2,15 @@ extends Parallax2D
 #really bad workaround script to try and fix major offest issues dealing with parallax
 func _ready() -> void:
 	for child in get_children():
-		print(child.position)
-		print(child)
-		child.position *= scroll_scale.x
-		print(child.position)
+		if child is ColorRect:
+			print(child)
+			print(child.position)
+			child.position.x = (child.position.x - child.size.x/2) / scroll_scale.x
+			child.position.y = (child.position.y - child.size.y/2) / scroll_scale.y
+			print(child.position)
+		else:
+			print(child)
+			print(child.position)
+			child.position /= scroll_scale
+			print(child.position)
 	print("Parallax positions baked.")
