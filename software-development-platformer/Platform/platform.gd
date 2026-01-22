917d84382ba2ad95c8e7ce843fb62f173f2e1b5f
Braedon <bpk07@icloud.com>
2026-01-22 09:49:07 -0500

    Platform border texture work
    Border texture doesnt warp, generates individual line children

--- software-development-platformer/Platform/platform.gd
+++ software-development-platformer/Platform/platform.gd
@@ -13,7 +13,8 @@ extends StaticBody2D
 
 @onready var polygon_2d = $Polygon2D
 @onready var collision_polygon_2d = $CollisionPolygon2D
-@onready var border_line_2d = $BorderLine2D
+#@onready var border_line_2d = $BorderLine2D
+@onready var border_lines = $BorderLines
 @onready var rectangle_outline = $RectangleOutline
 
 enum EditMode {RECTANGLE, POLYGON}
@@ -24,6 +25,8 @@ enum EditMode {RECTANGLE, POLYGON}
 @export var points : PackedVector2Array
 @export var startPosition : Vector2
 @export var sizeVector : Vector2 = Vector2(400,50)
+@export var texturePath : String = "res://.godot/imported/MissingTexture.png-22b33255012559b00bdd8aa9710640d6.ctex"
+@export var textureWidth : float = 10
 
 func _update_editor_visibility() -> void:
 	if not collision_polygon_2d or not rectangle_outline:
@@ -39,36 +42,63 @@ func _update_editor_visibility() -> void:
 				rectangle_outline.visible = false
 	else:
 		polygon_2d.visible = true
-		border_line_2d.visible = true
+		#border_line_2d.visible = true
+		border_lines.visible = true
 		rectangle_outline.visible = false
 
 func _sync_shapes() -> void:
 	#stop if node deleted
-	if not rectangle_outline or not collision_polygon_2d or not polygon_2d or not border_line_2d:
+	if not rectangle_outline or not collision_polygon_2d or not polygon_2d or not border_lines:
 		return
 	if edit_mode == EditMode.RECTANGLE:
 		#grab data from rectangle_outline
 		var center = rectangle_outline.position
 		sizeVector = rectangle_outline.shape.size
-		var topLeftCorner = center - sizeVector/2
-		var topRightCorner = Vector2(center + Vector2(sizeVector.x/2, -sizeVector.y/2))
-		var bottomRightCorner = center + sizeVector/2
-		var bottomLeftCorner = Vector2(center + Vector2(-sizeVector.x/2, sizeVector.y/2))
-		#push points to polygon array
-		points = PackedVector2Array()
-		points.push_back(topLeftCorner)
-		points.push_back(topRightCorner)
-		points.push_back(bottomRightCorner)
-		points.push_back(bottomLeftCorner)
+		points = _return_rectangle_points(center, sizeVector)
 		#match array to actual collision_polygon_2d, then push to other nodes
 		collision_polygon_2d.polygon = points
 		polygon_2d.polygon = points
-		border_line_2d.points = points
+		_generate_rectangle_borders(center, sizeVector)
+		#border_line_2d.points = points
 	if edit_mode == EditMode.POLYGON:
 		polygon_2d.polygon = collision_polygon_2d.polygon
-		border_line_2d.points = collision_polygon_2d.polygon
+		#border_line_2d.points = collision_polygon_2d.polygon
 		#border_line_2d.points = points
 
+
+func _generate_rectangle_borders(center, sizeVector) -> void:
+	_clear_children(border_lines)
+	sizeVector += Vector2(-textureWidth, -textureWidth)
+	var border_points = _return_rectangle_points(center, sizeVector)
+	for i in border_points.size():
+		var line_points = PackedVector2Array()
+		line_points.push_back(border_points.get(i))
+		line_points.push_back(border_points.get((i + 1) % border_points.size()))
+		var line = Line2D.new()
+		line.points = line_points
+		line.texture = load(texturePath)
+		line.width = textureWidth
+		line.texture_mode = Line2D.LINE_TEXTURE_TILE
+		line.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+		border_lines.add_child(line)
+
+#takes a center and size of a rectangle and returns an array of its corners
+func _return_rectangle_points(center, sizeVector) -> PackedVector2Array:
+	var topLeftCorner = center - sizeVector/2
+	var topRightCorner = Vector2(center + Vector2(sizeVector.x/2, -sizeVector.y/2))
+	var bottomRightCorner = center + sizeVector/2
+	var bottomLeftCorner = Vector2(center + Vector2(-sizeVector.x/2, sizeVector.y/2))
+	#push points to polygon array
+	points = PackedVector2Array()
+	points.push_back(topLeftCorner)
+	points.push_back(topRightCorner)
+	points.push_back(bottomRightCorner)
+	points.push_back(bottomLeftCorner)
+	return points
+
+func _clear_children(parent_node) -> void:
+	for child in parent_node.get_children():
+		child.queue_free()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
