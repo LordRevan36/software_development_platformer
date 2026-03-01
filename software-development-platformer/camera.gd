39d431a53dd163b5df4a5393ef3df09d53d6ae44
WorseThanBetter <dereudav000@hsestudents.org>
2026-02-28 23:04:35 -0500

    file renames for windows compatibility, small camera fixes
    will continue to work on camera to fix it and make it work at level boundaries.

--- software-development-platformer/camera.gd
+++ software-development-platformer/camera.gd
@@ -11,9 +11,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if player.state == Player.State.ATTACK: #camera moves slower when attacking, fixes bug that let them move camera farther than intended
-		offset = offset.lerp(player.velocity/1.5+Vector2(0,-50), delta * lerp_speed/2)
+		offset = offset.move_toward(player.velocity/1.5+Vector2(0,-50), delta * 100)
 	else:
-		offset = offset.lerp(player.velocity/1.5+Vector2(0,-50), delta * lerp_speed)
+		offset = offset.move_toward(player.velocity/1.5+Vector2(0,-50), delta * 100)
 	#prevents camera from going out of bounds. BROKEN
-	offset.x = clamp(offset.x+player.position.x, limit_left, limit_right)-player.position.x
-	offset.y = clamp(offset.y+player.position.y, limit_top, limit_bottom)-player.position.y
+	#offset.x = clamp(offset.x+player.position.x, limit_left, limit_right)-player.position.x
+	#offset.y = clamp(offset.y+player.position.y, limit_top, limit_bottom)-player.position.y
